908bb9656a821a7e33422baead6b39f4d03d8c63
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-13 08:53:51 +0545

    repeating animations/yoyo

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 gsap.to("#box1",{
     x:1200,
     duration:2,
-    delay:1
+    delay:1,
+    rotate:360,
+    backgroundColor:"blue",
+    borderRadius:"50%",
+    scale:0.5,
+    repeat:-1,
+    yoyo:"true"
+    //repeat -1 for infite running
 })
 
 
-gsap.from("#box2",{
-    x:1200,
+// gsap.from("#box2",{
+//     x:1200,
+//     duration:2,
+//     delay:1,
+//     borderRadius:"50%",
+//     scale:0.5
+// })
+
+gsap.from("h1",{
+    opacity:0,
+    // x:1200,
+    y:30,
     duration:2,
-    delay:1
+    delay:1,
+    stagger:0.3,
 })
