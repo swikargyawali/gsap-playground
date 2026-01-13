597b6df801e164c0a99790d20fe096928624465b
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-13 09:29:11 +0545

    gsap timeline

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 gsap.to("#box1",{
-    x:1200,
-    duration:2,
+    x:1500,
+    duration:1.5,
     delay:1,
     rotate:360,
     backgroundColor:"blue",
     borderRadius:"50%",
     scale:0.5,
-    repeat:-1,
-    yoyo:"true"
+    // repeat:-1,
+    // yoyo:"true"
     //repeat -1 for infite running
 })
 
 
-// gsap.from("#box2",{
-//     x:1200,
+gsap.to("#box2",{
+    x:1500,
+    rotate:360,
+    duration:1.5,
+    delay:2.5,
+    borderRadius:"50%",
+    scale:0.5
+})
+
+gsap.to("#box3",{
+    x:1500,
+    duration:1.5,
+    delay:4,
+    rotate:360,
+    scale:0.5,
+    borderRadius:"50%"
+})
+
+// gsap.from("h1",{
+//     opacity:0,
+//     // x:1200,
+//     y:30,
 //     duration:2,
 //     delay:1,
-//     borderRadius:"50%",
-//     scale:0.5
+//     stagger:0.3,
 // })
-
-gsap.from("h1",{
-    opacity:0,
-    // x:1200,
-    y:30,
-    duration:2,
-    delay:1,
-    stagger:0.3,
-})
