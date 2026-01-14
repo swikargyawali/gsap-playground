f97477981345afb87233949226e8dd81bddf9ec5
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-14 18:29:59 +0545

    scrollTrigger is working

--- scroll-trigger/script.js
+++ scroll-trigger/script.js
@@ -7,9 +7,11 @@ gsap.from("#page1 #box",{
 })
 
 gsap.from("#page2 #box",{
+    //we don't know when user scroll so we use smooth scrolling!!!!!
     scale:0,
-    delay:3,
     duration:2,
-    rotate:360
+    delay:1,
+    rotate:360,
+    scrollTrigger:"#page2 #box"
     
 })
